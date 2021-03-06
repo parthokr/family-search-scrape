af9f5177590f64a94b8b8b0f658392357fdd4778
ParthoKR <partho.rajvor2016@gmail.com>
2021-03-07 03:56:58 +0600

    Added multiprocessing

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from family_search_scrapper import FamilySearchScrapper
 from read_input import ReadInput
+import multiprocessing
 class Run(ReadInput):
-    def __init__(self, filename):
+    def __init__(self, filename, start, end):
         self.fscrapper = FamilySearchScrapper(match = 'exact')
-        super(Run, self).__init__(filename)
+        super(Run, self).__init__(filename, start, end)
+        self.execute()
 
     def execute(self):
         for row in self.reader:
@@ -18,6 +20,19 @@ class Run(ReadInput):
                 pass
                 
 if __name__ == "__main__":
-    obj = Run('input/Name_csv.csv')
-    obj.execute()
+
+    p1 = multiprocessing.Process(target=Run, args=('input/Name_csv.csv', 0, 10)) 
+    p2 = multiprocessing.Process(target=Run, args=('input/Name_csv.csv', 10, 20))
+    p3 = multiprocessing.Process(target=Run, args=('input/Name_csv.csv', 20, 30 ))
+    
+
+    p1.start()
+    p2.start()
+    p3.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+
+    print("Done")
     
