65f6e544d1fd50f953379374170017dc8d0b341d
ParthoKR <partho.rajvor2016@gmail.com>
2021-03-07 21:19:30 +0600

    Generating log file

--- family_search_scrapper.py
+++ family_search_scrapper.py
@@ -9,8 +9,21 @@ import requests
 
 from fs_parser.pr_parser import PRParser
 
+# from write_excel import WriteExcel
+from openpyxl import load_workbook, Workbook
+
 class FamilySearchScrapper(PRParser):
     def __init__(self, **kwargs):
+        # self.wx = WriteExcel()
+        self.count = 0
+        self.filename = kwargs.get('filename', None)
+        self.id = kwargs.get('id', None) # id for writing logs
+
+        if (self.filename is None):
+            raise Exception('Please provide a filename')
+
+        wb = Workbook()
+        wb.save(self.filename)
         self.match = kwargs.get('match', 'exact')
         self.firstname = kwargs.get('firstname', '').replace(' ', '%20')
         self.surname = kwargs.get('surname', '').replace(' ', '%20')
@@ -47,28 +60,40 @@ class FamilySearchScrapper(PRParser):
             raise Exception("Must have firstname or surname")
 
         if self.match == 'approximate':
-            self.search_url = f"https://www.familysearch.org/search/record/results?q.givenName=f{self.firstname}&q.surname={self.surname}&f.collectionId=2125045&count=20&offset=0&m.defaultFacets=on&m.queryRequireDefault=on&m.facetNestCollectionInCategory=on"
+            self.search_url = f"https://www.familysearch.org/service/search/hr/v2/personas?q.givenName={self.firstname}&q.surname={self.surname}&f.collectionId=2125045&count=20&offset=0&m.defaultFacets=on&m.queryRequireDefault=on&m.facetNestCollectionInCategory=on"
         elif self.match == "exact":
             self.search_url = f"https://www.familysearch.org/service/search/hr/v2/personas?q.givenName={self.firstname}&q.givenName.exact=on&q.surname={self.surname}&q.surname.exact=on&f.collectionId=2125045&count=20&offset=0&m.defaultFacets=on&m.queryRequireDefault=on&m.facetNestCollectionInCategory=on"
         else:
             raise Exception("Unknown match criteria")
 
-        start = time.time()
+        # start = time.time()
 
         test = self.req.get(self.search_url)
         source = test.text
 
         links = re.findall(r'https://www.familysearch.org/ark:/[0-9]+/[0-9]+:[0-9]+:[\w-]+', source)
-
-        # print(links)
-        try:
-            print(links[0])
-        except:
-            print(f"Not found for {self.firstname}")
+        if (len(links) == 0):
+            print(f"Not found for {self.firstname.replace('%20',' ')}")
+            log_file = open(f'log/logs.txt', 'a+')
+            log_file.write(f'[{self.id}]     Status: Failed to fetch\n')
+            log_file.close()
             return
 
+        
         fssessionid = self.req.cookies.get_dict()['fssessionid']
 
+
+        """
+            Sample: 
+                :authority: www.familysearch.org
+                :method: GET
+                :path: /ark:/61903/1:1:QVBP-VJ4W
+                :scheme: https
+                accept: application/x-gedcomx-v1+json, application/json
+                accept-encoding: gzip, deflate, br
+                accept-language: en
+                authorization: Bearer 907c9295-69ad-4841-85c5-e5d18992ba4c-prod
+        """
         data_header = {
             'Accept' : 'application/x-gedcomx-v1+json, application/json',
             'Accept-Encoding' : 'gzip, deflate, br',
@@ -80,14 +105,22 @@ class FamilySearchScrapper(PRParser):
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-origin'
         }
+        for link in links[0:1]:
+            start = time.time()
+            data = self.req.get(links[0], headers = data_header)
+            super().__init__(data.text)
 
-        data = self.req.get(links[0], headers = data_header)
-        # print(data.text)
+            wb = load_workbook(self.filename)
+            # Select First Worksheet
+            ws = wb.worksheets[0]
+            # Append 2 new Rows - Columns A - D
+            ws.append(super().get_data())
+            print(self.get_data())
+            wb.save(self.filename)
+            self.count += 1
 
-        super().__init__(data.text)
-        end = time.time()
-
-        print(end - start)
+            end = time.time()
+            print(end - start)
 
     def get_data(self):
         return super().get_data()
