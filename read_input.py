520fa22bca2d942af501756d33ac2366d9316e45
ParthoKR <partho.rajvor2016@gmail.com>
2021-03-07 21:19:48 +0600

    Reading CSV

--- read_input.py
+++ read_input.py
@@ -1,13 +1,14 @@
 import csv
 class ReadInput:
-    def __init__(self, filename):
+    def __init__(self, filename, start, end):
         filename = open(filename, 'r')
-        self.reader = list(csv.reader(filename))[1:10]
-        for row in self.reader:
-            print(row)
+        print("Reading CSV...")
+        self.reader = list(csv.reader(filename))[start:end]
+        # for row in self.reader:
+        #     print(row)
 
     # def get_data(self):
     #     return self.reader
 
 if __name__ == '__main__':
-    obj = ReadInput('input/Name_csv.csv')
+    obj = ReadInput('input/Name_csv.csv', 0, 10)
